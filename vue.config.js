35922c8fce50c3474b63b94035043864d6b8659f
prstorero <prssecustom94@gmail.com>
2022-12-26 12:53:43 -0600

    1) Remove base from the config as it's an older version of publicPath.
    2) Update publicPath to just / as that's where
    resources are located

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/prstorero.github.io/',
-  publicPath: '/prstorero.github.io',
+  publicPath: '/',
   transpileDependencies: true
 })
