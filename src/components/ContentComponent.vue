a100c0d4ba75fcc2dea8a67b46a342dac9adb0c9
prstorero <prssecustom94@gmail.com>
2022-12-26 13:47:43 -0600

    Remove the sidebar

--- src/components/ContentComponent.vue
+++ src/components/ContentComponent.vue
@@ -8,10 +8,6 @@
       <Hobbies />
       <AboutSite />
     </div>
-    <div v-auto-animate class="sidebar-container">
-      <a href="mailto:prssecustom94@gmail.com" class="sidebar">prssecustom94@gmail.com</a>
-      <div class="bar"></div>
-    </div>
   </div>
 </template>
 
@@ -48,43 +44,6 @@ export default {
   /* Allocate the same number of columns to each content child */
   .container > * {
     grid-column: 2/12;
-  }
-
-  .sidebar-container {
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    margin-right: 2rem;
-  }
-  .sidebar {
-    /* Make our text vertical */
-    writing-mode: vertical-lr;
-
-    /* Space between text and line */
-    margin-bottom: 1rem;
-
-    /* Font styling */
-    color: var(--white);
-
-    transition: all 0.25s ease-in;
-  }
-  .sidebar:hover {
-    transform: translateY(-3px);
-    color: var(--color-primary-light);
-  }
-  .bar:after {
-    content: "";
-    display: block;
-    width: 1px;
-    height: 90px;
-    margin: 0px auto;
-    background-color: var(--color-primary-light);
-  }
-
-  @media screen and (max-width: 600px) {
-    .sidebar-container {
-      display: none;
-    }
   }
 
   ::v-deep .section {
